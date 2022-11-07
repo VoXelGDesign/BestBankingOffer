9b2955d60dc3ddecb47f10e14c3bd8f24103c61e
VoXelGDesign <trynewgame@gmail.com>
2022-11-07 19:24:10 +0100

    1.0-FINAL
    Removed javadoc

--- BestBankingOffer/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.polsl.student.jakunow552.bestbankingoffer"},{"l":"pl.polsl.student.jakunow552.bestbankingoffer.controller"},{"l":"pl.polsl.student.jakunow552.bestbankingoffer.model"},{"l":"pl.polsl.student.jakunow552.bestbankingoffer.view"}];updateSearchResults();
